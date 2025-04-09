9b82bd161c0110705e38d53f29bf170c8afa0f96
Andrii Zatsepin <uzatsepin@gmail.com>
2025-04-09 21:46:30 +0300

    initial commit-2

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,11 @@ export default defineConfig({
         },
     },
     server: {
+        host: true,
+        port: 5174,
         allowedHosts: true,
+        watch: {
+            usePolling: true,
+        },
     },
 })
